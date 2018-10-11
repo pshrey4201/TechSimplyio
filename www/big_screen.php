dba7b3fab6353ba97648114ade0021049f5c95ef
pshrey4201 <patelshrey4201@gmail.com>
2018-10-10 20:48:20 -0400

    made the cards and made them mobile compatible

--- www/big_screen.php
+++ www/big_screen.php
@@ -13,3 +13,50 @@
   <img id="planet" src="./img/planet.png"></img>
   <div id="threeJsContainer"></div>
   <script src="./js/three.js"></script>
+<div id="card" class="articles">
+<img id="icon" src="./img/security.svg"></img>
+<p>Security tips</p>
+<ul>
+  <li>Avoid random links.</li>
+  <li>Don't tell anyone your password.</li>
+  <li>Change password every 30-90 days.</li>
+  <li>Do not go websites that offer "free downloads"!</li>
+  <li>Use an adblocker, I recommend "Adguard", it is free and safe!</li>
+  <li>Read the news about data breaches, to see if your account is affected.</li>
+  <li>Anything promising virtual currency for a video game is a scam, do not fall for it!</li>
+</ul>
+</div>
+<div id="card2" class="articles">
+<img id="icon" src="./img/socialMedia.svg"></img>
+<p>Social media tips</p>
+<ul>
+  <li>Click links with caution.</li>
+  <li>Do not post obsence pictures.</li>
+  <li>Enable two-factor authentication.</li>
+  <li>Do not post personal information.</li>
+  <li>Do not tell anyone your password!</li>
+  <li>Make sure you have a strong password.</li>
+  <li>If you have a private profile be careful who you accepts and follows.</li>
+  </ul>
+</div>
+<div id="card3" class="articles">
+<img id="icon" src="./img/http.svg"></img>
+<p>Internet safety tips</p>
+<ul>
+  <li>Set social media accounts to private.</li>
+  <li>Don't talk to people you do not know online.</li>
+  <li>Do not leak your address or any personal information.</li>
+  <li>Use psuedo names, do not use a real or relevant name.</li>
+  <li>Don't post pictures that you wouldn't want your guardians to see.</li>
+</ul>
+</div>
+<div id="card4" class="articles">
+<img id="icon" src="./img/aboutUs.svg"></img>
+<p>About us</p>
+<ul>
+  <li>Back-end: Caleb</li>
+  <li>Front-end: Shrey</li>
+  <li>Program: Excel Tecc</li>
+  <li>Grade-level: Seniors </li>
+  <li>Graphics/Front-end: Gabe</li>
+</div>
